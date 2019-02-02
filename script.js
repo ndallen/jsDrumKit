e85963b541d18bd42ddcc5bae739e3107309769f
ndallen <ecommerceevolution@gmail.com>
2019-02-02 13:06:55 +0700

    audio.play() connected and current time set to 0

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  if(!audio) return ; // this will stop function from running
+  audio.currentTime = 0;
+  audio.play();
 });
