a8b25401087b72d49da04b90a1f2820aeaa778dd
ndallen <ecommerceevolution@gmail.com>
2019-02-02 13:54:27 +0700

    key events added and project complete

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return ; // this will stop function from running
-  audio.currentTime = 0;
+  audio.currentTime = 0; //rewind to the start
   audio.play();
+  key.classList.add('playing');
+  
 });
+function removeTransition(e){
+  if (e.propertyName !== 'transform') return;  //skip it if not a transform
+  this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
